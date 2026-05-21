d736cbad2c25e111deff5cf39d8b799466ef16e2
MISAEL CORONEL <misaelcoronel678@gmail.com>
2026-05-21 23:55:29 +0000

    Error: arregle un error de logica en donde el programa no realizaba bien las restricciones

--- Ejercicios_de_logica/validador_de_contraseñas.py
+++ Ejercicios_de_logica/validador_de_contraseñas.py
@@ -17,21 +17,21 @@ el largo).
 decile exactamente en qué falló.'''
 
 contraseña = input("Ingrese la contraseña: ")
+minusc = contraseña.lower()
 contraseña_caracteres = len(contraseña)
-palabras_no_admitidas = contraseña.count ("abc")
-palabras_no_admitidas2 = contraseña.count ("123")
+palabras_no_admitidas = minusc.count ("abc")
+palabras_no_admitidas2 = minusc.count ("123")
 lista_caracteres = list(contraseña)
 final_contraseña = lista_caracteres[-1]
 caracter_1 = "!"
 caracter_2 = "."
 
+
 if contraseña_caracteres < 8:
     print ("la contraseña es demaciado corta Ingrese una con almenos 8 caraceres")
-elif palabras_no_admitidas > 0 and palabras_no_admitidas2 > 0:
+elif palabras_no_admitidas > 0 or palabras_no_admitidas2 > 0:
     print ("No se permiten tener abc ni 123 en la contraseña")
 elif final_contraseña != caracter_1 and final_contraseña != caracter_2:
     print("La contraseña debe de terminar en ! o en .")
 else:
     print("contraseña valida")
-
-#Falta corregir algo en el elif de la linea 30 no tiene que permitir palabras con "abc" ni "123"
