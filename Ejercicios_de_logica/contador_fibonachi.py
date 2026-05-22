d4f7b2dc1e232201800f00c55a970d18d16d5ccc
MISAEL CORONEL <misaelcoronel678@gmail.com>
2026-05-22 22:20:30 +0000

    archivo: cree un nuevo archivo de la leccion de hoy Operaciones Aritmeticas

---
+++ Ejercicios_de_logica/contador_fibonachi.py
@@ -0,0 +1,6 @@
+'''creando una funcion que muestre la serie fibonaci 
+entre el 0 y el numero dado'''
+
+def succesion_fibonaci (num):
+    for i in ra
+
---
+++ Operadores_Aritméticos/op_aritmeticos.py
@@ -0,0 +1,20 @@
+#suma y resta (+ y -)
+suma = 12 + 5
+resta = 12 - 5
+
+#Multiplicacion y Division (* y /)
+multi = 12 * 5
+division = 12 / 5
+
+#Potenciacion (exponente) (**)
+exponente = 12 ** 5
+
+#division baja (//)
+division_baja = 12 // 5 #devuelve entero redondeado hacia abajo
+
+#resto o modulo
+resto = 12 % 5
+
+tipo_de_dato = type (division_baja) #type (dato) nos devuelve que tipo de dato es
+
+print (division_baja)
